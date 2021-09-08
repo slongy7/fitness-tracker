4ae757647d08958d13d4ffbe2b1905c936f85eb2
slongy7 <79759725+slongy7@users.noreply.github.com>
2021-09-08 10:55:41 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3007;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    userNewUrlParser: true,
+    useFindAndModify: false
+});
+
+require('./routes/htmlroutes')(app);
+require('./routes/apiroutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
